fb422662e08e54c3118d09198b7678e13262a3e7
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-05 10:54:54 +0530

    Delete init.py

--- handlers/init.py
+++
@@ -1,8 +0,0 @@
-from .start import register as register_start
-from .upload import register as register_upload
-from .admin import register as register_admin
-
-def register_handlers(app):
-    register_start(app)
-    register_upload(app)
-    register_admin(app)
