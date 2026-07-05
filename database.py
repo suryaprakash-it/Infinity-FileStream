10fa756a75cdc420e300f38595fe349e1311a72d
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-05 11:05:07 +0530

    Update database.py

--- database.py
+++ database.py
@@ -9,35 +9,22 @@ files = db["files"]
 users = db["users"]
 
 
+# ==========================
+# FILES
+# ==========================
+
 async def add_file(data: dict):
     await files.insert_one(data)
-
-
-async def get_file(file_id: str):
-    return await files.find_one({"_id": file_id})
-
-
-async def add_user(user_id: int):
-    if not await users.find_one({"_id": user_id}):
-        await users.insert_one({"_id": user_id})
-
-
-async def total_users():
-    return await users.count_documents({})
-
-
-async def total_files():
-    return await files.count_documents({})
-
-
 
 
 async def get_file(file_code: str):
     return await files.find_one({"_id": file_code})
 
 
-async def delete_file(file_code: str):
-    await files.delete_one({"_id": file_code})
+async def get_file_by_unique_id(file_unique_id: str):
+    return await files.find_one(
+        {"file_unique_id": file_unique_id}
+    )
 
 
 async def update_file(file_code: str, data: dict):
@@ -45,3 +32,31 @@ async def update_file(file_code: str, data: dict):
         {"_id": file_code},
         {"$set": data}
     )
+
+
+async def delete_file(file_code: str):
+    await files.delete_one({"_id": file_code})
+
+
+async def total_files():
+    return await files.count_documents({})
+
+
+# ==========================
+# USERS
+# ==========================
+
+async def add_user(user_id: int):
+
+    if await users.find_one({"_id": user_id}):
+        return
+
+    await users.insert_one(
+        {
+            "_id": user_id
+        }
+    )
+
+
+async def total_users():
+    return await users.count_documents({})
