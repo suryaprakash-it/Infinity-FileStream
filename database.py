e4d4ae6f85e5feaad9218f31ce9fefb0aefc2fa2
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-05 10:26:43 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,30 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import Config
+
+client = AsyncIOMotorClient(Config.MONGO_URI)
+
+db = client["InfinityFileStream"]
+
+files = db["files"]
+users = db["users"]
+
+
+async def add_file(data: dict):
+    await files.insert_one(data)
+
+
+async def get_file(file_id: str):
+    return await files.find_one({"_id": file_id})
+
+
+async def add_user(user_id: int):
+    if not await users.find_one({"_id": user_id}):
+        await users.insert_one({"_id": user_id})
+
+
+async def total_users():
+    return await users.count_documents({})
+
+
+async def total_files():
+    return await files.count_documents({})
