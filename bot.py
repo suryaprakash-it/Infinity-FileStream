aca1b3fd5ef30827d40d42524d91c5aad852f7e1
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-06 23:58:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-from telegram_client import telegram
+from telegram_client import bot
 from handlers import register_handlers
 
-register_handlers(telegram)
+register_handlers(bot)
 
 print("🤖 Infinity FileStream Bot Started!")
 
-telegram.run()
+bot.run()
