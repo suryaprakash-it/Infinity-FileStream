bc894abd26dfdf657f68b094d9ca2f3e69392406
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-06 23:59:38 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,3 +1,47 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from database import get_file
+
+app = FastAPI(
+    title="Infinity FileStream",
+    version="1.0.0"
+)
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def home():
+    return {
+        "status": "online",
+        "project": "Infinity FileStream"
+    }
+
+
+@app.get("/file/{file_code}", response_class=HTMLResponse)
+async def file_page(request: Request, file_code: str):
+
+    file = await get_file(file_code)
+
+    if not file:
+        return HTMLResponse(
+            "<h2>❌ File Not Found</h2>",
+            status_code=404
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "download.html",
+        {
+            "file_name": file["file_name"],
+            "file_size": file["file_size"],
+            "file_code": file_code
+        }
+    )
+
+
 @app.get("/download/{file_code}")
 async def download_file(file_code: str):
 
