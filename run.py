84880a42a967a0d9eb4b4aaeca7d3645472a6046
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-07 00:16:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,10 @@
 import subprocess
-import threading
 
-
-def start_bot():
-    subprocess.run(["python", "bot.py"])
-
-
-def start_web():
-    subprocess.run(["uvicorn", "web:app", "--host", "0.0.0.0", "--port", "8080"])
-
-
-bot_thread = threading.Thread(target=start_bot)
-web_thread = threading.Thread(target=start_web)
-
-bot_thread.start()
-web_thread.start()
-
-bot_thread.join()
-web_thread.join()
+subprocess.run([
+    "uvicorn",
+    "web:app",
+    "--host",
+    "0.0.0.0",
+    "--port",
+    "8080"
+])
